ccd35b69e0a2bc05ec1e65604d2df379d15cd257
Suraksha Vidyarthi <vidyarthi@us.ibm.com@Surakshas-MBP.attlocal.net>
2019-08-07 16:48:41 -0700

    Fixed the compilation issues with terraform plan

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,9 @@
 output "cluster_master_ip" {
   value = "${element(compact(concat(ibm_is_floating_ip.fipmaster.address)),0)}"
 }
-output "symphony_dehost_ip" {
-  value = "${join(" ", concat(ibm_is_floating_ip.fipdehosts.address))}"
-}
+#output "symphony_dehost_ip" {
+ # value = "${join(" ", concat(ibm_is_floating_ip.fipdehosts.address))}"
+#}
 output "cluster_web_interface" {
   value = "https://${element(compact(concat(ibm_is_floating_ip.fipmaster.address)),0)}:8443/platform"
 }
